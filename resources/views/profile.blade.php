95413d648842d60b8d98303543fa5d540d901747
Carla Cupani <cpnclp00@gmail.com>
2024-09-03 21:45:51 +0200

    Changes to be committed:

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -65,32 +65,42 @@
 
     <!--PROFILE-->
     <section class="profile-section">
-        <div class="profile">
-            <div class="profile-pic">
-
+        <h2>I miei dati:</h2>
+        <div class="profile-card">
+            <div class="profile-card-body">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Nome</td>
+                            <td>:</td>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Cognome</td>
+                            <td>:</td>
+                            <td>{{ $user->surname }}</td>
+                        </tr>
+                        <tr>
+                            <td>Username</td>
+                            <td>:</td>
+                            <td>{{ $user->username }}</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-
-            <span class="profile-name">
-                Name: {{ $user->name }}
-            </span>
-            <span class="profile-name">
-                Surname: {{ $user->surname }}
-            </span>
-            <span class="profile-name">
-                Username: {{ $user->username }}
-            </span>
-            <span class="profile-name">
-                Email: {{ $user->email }}
-            </span>
-
-        </div>
-        <div class="profile-buttons">
-            <a href="{{ URL::to("edit_profile") }}">
-                <button class="edit-account-btn" id="edit-account-btn">Modifica</button>
-            </a>
-            <a href="{{ URL::to("logout") }}">
-                <button class="logout-btn" id="logout-btn">Logout</button>
-            </a>
+            <div class="profile-buttons">
+                <a href="{{ URL::to("edit_profile") }}">
+                    <button class="edit-account-btn" id="edit-account-btn">Modifica</button>
+                </a>
+                <a href="{{ URL::to("logout") }}">
+                    <button class="logout-btn" id="logout-btn">Logout</button>
+                </a>
+            </div>
         </div>
     </section>
 
